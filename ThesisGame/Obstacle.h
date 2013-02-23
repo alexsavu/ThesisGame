df30cb05c52d24fb0df45a7ad644be30a4ccc03f
Alex Savu <alexsavu@hotmail.com>
2013-02-23 19:11:53 +0100

    added random spawning obstacles

---
+++ ThesisGame/Obstacle.h
@@ -0,0 +1,14 @@
+//
+//  Obstacle.h
+//  ThesisGame
+//
+//  Created by Alex Savu on 2/23/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "GameCharacter.h"
+
+@interface Obstacle : GameCharacter
+
+@end
